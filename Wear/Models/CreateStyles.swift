17d4f775f010ff09a7fe1de81be99b6e9a928341
melodyfs <melodyfs@users.noreply.github.com>
2017-06-30 11:52:39 -0700

    Import all weather states/descriptions

--- Wear/Models/CreateStyles.swift
+++ Wear/Models/CreateStyles.swift
@@ -13,15 +13,107 @@ class CreateStyles {
     //MARK: - Enums
     
     enum StyleFromDecription {
+        
+        //thunderstorm
+        case thunderstormWithLightRain
+        case thunderstormWithRain
+        case thunderstormWithHeavyRain
+        case lightThunderstorm
+        case thunderstorm
+        case heavyThunderstorm
+        case raggesdThunderstorm
+        case thunderstormWithLightDrizzle
+        case thunderstormWithDrizzle
+        case thunderstormWithHeavyDrizzle
+        
+        //drizzle
+        case lightIntensityDrizzle
+        case drizzle
+        case heavyIntensityDrizzle
+        case lightIntensityDrizzleRain
+        case drizzleRain
+        case heavyIntensityDrizzleRain
+        case showerRainAndDrizzle
+        case heavyShowerAndDrizzle
+        case showerRainDrizzle
+        
+        //rain
+        case lightRain
+        case moderateRain
+        case heavyIntensityRain
+        case veryHeavyRain
+        case extremeRain
+        case freezingRain
+        case lightIntensityShowerRain
+        case showerRain
+        case heavyIntensityShowerRain
+        case raggedShowerRain
+        
+        //snow
+        case lightSnow
+        case snow
+        case heavySnow
+        case sleet
+        case showerSleet
+        case lightRainAndSnow
+        case rainAndSnow
+        case lightShowerSnow
+        case showerSnow
+        case heavyShowerSnow
+        
+        //atmosphere
         case mist
-        case rain
+        case smoke
+        case haze
+        case sandAndDustWhirls
+        case fog
+        case sand
+        case dust
+        case volcanicAsh
+        case squalls
+        case tornado
+        
+        //clear
         case clearSky
+        
+        //clouds
+        case fewClouds
+        case scatteredClouds
+        case brokenClouds
+        case overcastClouds
+        
+        //extreme
+        case tropicalStorm
+        case hurricane
+        case cold
+        case hot
+        case windy
+        case hail
+        
+        //additional
+        case calm
+        case lightBreeze
+        case gentleBreeze
+        case moderateBreexe
+        case freshBreexe
+        case strongCreeze
+        case highWindNearGale
+        case gale
+        case severeGale
+        case storm
+        case violentStorm
+        
+        //test code: generalization key word
+//        case mist
+        case rain
+//        case clearSky
+    
         
     }
     
     enum StyleFromTemp {
         case below60
-        case from64To70
+        case from60To70
         case above70
     }
     
@@ -34,64 +126,88 @@ class CreateStyles {
     enum UpperBody: String {
         case longSleeveShirt = "long Sleeve Shirt"
         case tShirt = "t Shirt"
+        
         case jacket = "jacket"
-        case rainCoat = "rain coat "
+        case sweater = "sweater"
+        case trenchCoat = "trench coat"
+        
+        case dress = "dress"
         
     }
     
     enum LowerBody: String {
         case pants = "pants"
         case shorts = "shorts"
+        case jeans = "jeans"
+        case leggings = "leggings"
         
+        case skirt = "skirt"
     }
     
-    enum Shoes: String {
+    enum Feet: String {
         case sneakers = "snaakers"
         case boots = "boots"
         case sandals = "sandals"
         case rainBoots = "rain boots"
+        
+        case socks = "socks"
     
     }
     
     enum Accessories: String {
         case umbrella = "umbrella"
+        case rainCoat = "rain coat"
+        case scarf = "scarf"
+        case gloves = "gloves"
+        case helmet = "helmet"
         
     }
     
     //MARK: - Actions
     
     //a function that tells which code path to go when a style is called
-    func createStyle(type: StyleFromDecription) {
+    func createStyleByDescription(type: StyleFromDecription) {
         switch type {
         case .mist:
-            print("\(Head.hat), \(UpperBody.longSleeveShirt), \(LowerBody.pants), \(Shoes.sneakers), \(UpperBody.jacket)")
+            print("\(Head.hat), \(UpperBody.longSleeveShirt), \(LowerBody.pants), \(Feet.sneakers), \(UpperBody.jacket)")
         case .rain:
-            print("\(Head.cap), \(UpperBody.tShirt), \(LowerBody.pants), \(Shoes.boots), \(Accessories.umbrella)")
+            print("\(Head.cap), \(UpperBody.tShirt), \(LowerBody.pants), \(Feet.boots), \(Accessories.umbrella)")
         case .clearSky:
-            print("\(Head.hat), \(UpperBody.tShirt), \(LowerBody.shorts), \(Shoes.sandals)")
+            print("\(Head.hat), \(UpperBody.tShirt), \(LowerBody.shorts), \(Feet.sandals)")
         default:
-            print("Style yet to be created")
+            print("")
+
         }
-        
-    
 
     }
     
-    func createStyle(type: StyleFromTemp) {
+    func createStyleByTemp(type: StyleFromTemp) {
         switch type {
         case .below60:
-            print("\(Head.hat), \(UpperBody.longSleeveShirt), \(LowerBody.pants), \(Shoes.sneakers), \(UpperBody.jacket)")
-        case .from64To70:
-            print("\(Head.cap), \(UpperBody.tShirt), \(LowerBody.pants), \(Shoes.boots), \(Accessories.umbrella)")
+            print("\(Head.hat), \(UpperBody.longSleeveShirt), \(LowerBody.pants), \(Feet.sneakers), \(UpperBody.jacket)")
+        case .from60To70:
+            print("\(Head.cap), \(UpperBody.tShirt), \(LowerBody.pants), \(Feet.boots), \(Accessories.umbrella)")
         case .above70:
-            print("\(Head.hat), \(UpperBody.tShirt), \(LowerBody.shorts), \(Shoes.sandals)")
-        default:
-            print("Style yet to be created")
+            print("\(Head.hat), \(UpperBody.tShirt), \(LowerBody.shorts), \(Feet.sandals)")
+        
         }
         
         
         
     }
+    
+    
+    //combining both temp and description to generate outfit combos
+//    class func giveSuggestion(description ds: String, temperature temp: Int) {
+//        let style = CreateStyles()
+//        let description = WeatherData.shared.description
+//        let temp = WeatherData.shared.temperature
+//        
+//        
+//        
+//    }
+    
+    
     
     //call the createStyle func in enum based on weather description
     class func getStyle(description: String) {
@@ -99,15 +215,15 @@ class CreateStyles {
         let style = CreateStyles()
         
         if description.range(of: "mist") != nil {
-            style.createStyle(type: .mist)
+            style.createStyleByDescription(type: .mist)
         }
         
         if description.range(of: "rain") != nil {
-            style.createStyle(type: .rain)
+            style.createStyleByDescription(type: .rain)
         }
         
         if description.range(of: "clear sky") != nil {
-            style.createStyle(type: .clearSky)
+            style.createStyleByDescription(type: .clearSky)
             
         }
         
@@ -120,15 +236,15 @@ class CreateStyles {
         let style = CreateStyles()
         
         if temp < 60 {
-            style.createStyle(type:.below60)
+            style.createStyleByTemp(type:.below60)
         }
         
         if temp > 60 && temp < 70 {
-            style.createStyle(type: .from64To70)
+            style.createStyleByTemp(type: .from60To70)
         }
         
         if temp > 70 {
-            style.createStyle(type: .above70)
+            style.createStyleByTemp(type: .above70)
             
         }
         
