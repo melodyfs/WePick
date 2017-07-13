77e1484fa42647697cbb434000b3c0111bc4b4ce
melodyfs <melodyfs@users.noreply.github.com>
2017-07-13 15:37:20 -0700

    Create tab bar

---
+++ Wear/ViewControllers/SelectionTBC.swift
@@ -0,0 +1,44 @@
+//
+//  SelectionViewController.swift
+//  Wear
+//
+//  Created by Melody on 7/13/17.
+//  Copyright © 2017 Melody Yang. All rights reserved.
+//
+
+import UIKit
+
+class SelectionTBC: UITabBarController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self as UITabBarControllerDelegate
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SelectionTBC: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true
+    }
+}
